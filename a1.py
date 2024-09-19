ce82b19c993e0c2ced91066927e24aaed74f0a24
Daniel Vargas <dnvargas1@cps.edu>
2024-09-19 10:19:01 -0500

    finished

--- a1.py
+++ a1.py
@@ -155,6 +155,21 @@ def duck_duck_goose(lst: List[str]) -> List[str]:
     Returns:
         the resulting list after playing duck duck goose
     """
+    i = 0
+    current = "duck1"
+    while len(lst) > 2:
+        if current == "duck1":
+            current = "duck2"
+            i += 1
+        elif current == "duck2":
+            current = "goose"
+            i += 1
+        else:
+            current = "duck1"
+            lst.pop(i)
+        if i == len(lst):
+            i = 0
+    return lst
         
     
 
